ae4a2a44319f2fd49d7f7d61acc6cbb6361c350b
Nuno Dias <nfdias1998@gmail.com>
2022-07-07 16:23:39 +0100

    Added ignore patterns to telescope.nvim

--- nvim/.config/nvim/lua/nulldays/telescope.lua
+++ nvim/.config/nvim/lua/nulldays/telescope.lua
@@ -3,6 +3,10 @@ local previewers = require("telescope.previewers")
 require('telescope').setup {
     promp_prefix = ">",
     color_devicons = true,
+    file_ignore_patterns = {
+        ".git",
+        "node_modules",
+    }
 }
 require('telescope').load_extension('fzf')
 
